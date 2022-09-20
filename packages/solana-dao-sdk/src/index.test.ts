50475d4cd4a0555f984ac0ba7df36cdc3a6ec57d
Ariel Segura <ariel.segura@yahoo.com>
2022-09-20 22:30:52 +0200

    implement getDao

--- packages/solana-dao-sdk/src/index.test.ts
+++ packages/solana-dao-sdk/src/index.test.ts
@@ -1,8 +1,20 @@
 import { SolanaDao } from "./index";
+import { PublicKey } from "@solana/web3.js";
+import { BN } from "bn.js";
 
 describe("SolanaDao", () => {
-  test("calling getDaos return a truthy value", () => {
+  test("calling getDao return a value", async () => {
     const client = new SolanaDao();
-    expect(client.getDaos()).toBeTruthy();
+    const actualDao = await client.getDao(new PublicKey("5piGF94RbCqaogoFFWA9cYmt29qUpQejGCEjRKuwCz7d"));
+    const expectedDao = {
+        publicKey: new PublicKey("5piGF94RbCqaogoFFWA9cYmt29qUpQejGCEjRKuwCz7d"),
+        name: "Ukraine.SOL",
+        authority: new PublicKey("5We3g4zpinMcxSFrkvPWRzcSM5oRYxfWc9c8idefAQdi"),
+        communityMint: new PublicKey("ukrn3bJz9dzSN2VpF76ytpNcugXHEy4VuRou7bpizgF"),
+        councilMint: new PublicKey("cUAxX8fEXdisGSgRnfL4aH3WeomK5b277XqHHS4U9z7"),
+        minCommunityTokensToCreateGovernance: "1000000000000",
+        votingProposalCount: 0
+    };
+    expect(actualDao).toEqual(expectedDao);
   });
 });
