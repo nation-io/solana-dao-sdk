a3c506a274f82f7d43d6ea4670803b33a8145939
Ariel <arielsegura@users.noreply.github.com>
2022-09-26 21:48:53 +0200

    implement getDao (#1)

--- packages/solana-dao-sdk/src/index.ts
+++ packages/solana-dao-sdk/src/index.ts
@@ -1,36 +1,60 @@
+import { Connection, clusterApiUrl, PublicKey } from '@solana/web3.js';
+import { AccountType } from './internal/governance/model';
+import { RealmV2Serializer } from './internal/serialization';
+import BN from 'bn.js';
+
+/**
+ * Note: This interface is an abstraction introduced by the SDK so that consumers don't care about having to (de)serialize deprecated or unused fields
+ * or the fact that some fields are nester in other objects. 
+ * For more info, check the smart contract https://github.com/solana-labs/solana-program-library/blob/a179eba71e63ba18128cc466e3fd0f7535f4c5fd/governance/program/src/state/realm.rs#L133
+ */
 export type Dao = {
-  id: string;
+  publicKey: PublicKey;
   name: string;
+  // The current authority of the Realm
+  authority?: PublicKey;
+  // Mint address of the token to be used to represent voting power
+  communityMint: PublicKey;
+  // Optional mint address of the token to be used to represent voting power in the council
+  councilMint?: PublicKey;
+  // Min number of community tokens required to create a governance
+  minCommunityTokensToCreateGovernance: String; // TODO should be BN, but the test fails
+  // The number of proposals in voting state
+  votingProposalCount: number;
 };
 
-const mockDaos: Array<Dao> = [
-  {
-    id: "ukraine",
-    name: "Ukraine.SOL",
-  },
-  {
-    id: "mango-dao",
-    name: "Mango DAO",
-  },
-  {
-    id: "psy-finance",
-    name: "Psy Finance",
-  },
-  {
-    id: "grape",
-    name: "GRAPE",
-  },
-  {
-    id: "solend-dao",
-    name: "Solend DAO",
-  },
-];
-
 export class SolanaDao {
-  getDao(): Dao {
-    return mockDaos[0];
+  connection: Connection;
+  serializer = new RealmV2Serializer();
+  
+  constructor(connection?: Connection) {
+    this.connection = connection ? connection : new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+  }
+
+  async getDao(publicKey: PublicKey): Promise<Dao | null> {
+    const accountInfo = await this.connection.getAccountInfo(publicKey);
+    if (!accountInfo) {
+      return null;
+    }
+    const buffer: Buffer = accountInfo.data;
+    // first field must be the account type
+    const accountType = buffer[0];
+    if (accountType !== AccountType.RealmV2.valueOf()) {
+      return null;
+    }
+    const realm = this.serializer.deserialize(buffer);
+    
+    return {
+      publicKey: publicKey,
+      name: realm.name,
+      authority: realm.authority,
+      communityMint: realm.communityMint,
+      councilMint: realm.config.councilMint,
+      minCommunityTokensToCreateGovernance: realm.config.minCommunityTokensToCreateGovernance.toString(10),
+      votingProposalCount: realm.votingProposalCount
+    };
   }
   getDaos(): Array<Dao> {
-    return mockDaos;
+    throw new Error("Not implemented yet.");
   }
 }
