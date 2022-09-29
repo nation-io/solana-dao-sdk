2aa04d32828c0e762d60d4907d11e08a282bfb26
Ariel <arielsegura@users.noreply.github.com>
2022-09-29 15:38:01 +0200

    user anchor for serialization (#6)

--- packages/solana-dao-sdk/src/index.ts
+++ packages/solana-dao-sdk/src/index.ts
@@ -1,7 +1,8 @@
-import { Connection, clusterApiUrl, PublicKey } from '@solana/web3.js';
-import { AccountType } from './internal/governance/model';
+import { Connection, clusterApiUrl, PublicKey, ConfirmOptions, SendOptions, Signer, Transaction } from '@solana/web3.js';
 import { RealmV2Serializer } from './internal/serialization';
-import BN from 'bn.js';
+import { splGovernanceProgram, SplGovernance } from "@project-serum/spl-governance";
+import { Keypair } from "@solana/web3.js";
+import { Program, AnchorProvider, Wallet } from "@project-serum/anchor";
 
 /**
  * Note: This interface is an abstraction introduced by the SDK so that consumers don't care about having to (de)serialize deprecated or unused fields
@@ -18,30 +19,55 @@ export type Dao = {
   // Optional mint address of the token to be used to represent voting power in the council
   councilMint?: PublicKey;
   // Min number of community tokens required to create a governance
-  minCommunityTokensToCreateGovernance: String; // TODO should be BN, but the test fails
+  minCommunityWeightToCreateGovernance: String; // TODO should be BN, but the test fails
   // The number of proposals in voting state
   votingProposalCount: number;
 };
 
+class NoOpWallet extends Wallet {
+  constructor(){
+    // a key pair we don't use but it's necessary in the constructor
+    super(Keypair.generate());
+  }
+  signTransaction(tx: Transaction): Promise<Transaction> {
+    throw new Error('Method not implemented.');
+  }
+  signAllTransactions(txs: Transaction[]): Promise<Transaction[]> {
+    throw new Error('Method not implemented.');
+  }
+  get publicKey(): PublicKey {
+    return PublicKey.default;
+  }
+  
+}
+
+/**
+ * By default, Anchor expects a provider or default to Local which expects a valid wallet configured (i.e. node)
+ */
+class NoOpProvider extends AnchorProvider {
+  constructor(connection: Connection){
+    super(connection, new NoOpWallet(), AnchorProvider.defaultOptions())
+  }
+}
+
 export class SolanaDao {
   connection: Connection;
-  serializer = new RealmV2Serializer();
+  program: Program<SplGovernance>;
+  serializer: RealmV2Serializer;
   
   constructor(connection?: Connection) {
     this.connection = connection ? connection : new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+    this.program = splGovernanceProgram({provider: new NoOpProvider(this.connection)});
+    this.serializer = new RealmV2Serializer(this.program.coder);
   }
 
   async getDao(publicKey: PublicKey): Promise<Dao | null> {
+    
     const accountInfo = await this.connection.getAccountInfo(publicKey);
     if (!accountInfo) {
       return null;
     }
-    const buffer: Buffer = accountInfo.data;
-    // first field must be the account type
-    const accountType = buffer[0];
-    if (accountType !== AccountType.RealmV2.valueOf()) {
-      return null;
-    }
+    const buffer: Buffer = accountInfo.data;    
     const realm = this.serializer.deserialize(buffer);
     
     return {
@@ -50,7 +76,7 @@ export class SolanaDao {
       authority: realm.authority,
       communityMint: realm.communityMint,
       councilMint: realm.config.councilMint,
-      minCommunityTokensToCreateGovernance: realm.config.minCommunityTokensToCreateGovernance.toString(10),
+      minCommunityWeightToCreateGovernance: realm.config.minCommunityWeightToCreateGovernance.toString(10),
       votingProposalCount: realm.votingProposalCount
     };
   }
