bab6088ef0ec6be82d572097b00441806b889e0b
Ariel Segura <ariel.segura@yahoo.com>
2022-10-01 16:23:44 +0200

    Revert "user anchor for serialization (#6)"
    This reverts commit 2aa04d32828c0e762d60d4907d11e08a282bfb26.

--- packages/solana-dao-sdk/src/index.test.ts
+++ packages/solana-dao-sdk/src/index.test.ts
@@ -1,5 +1,6 @@
 import { SolanaDao } from "./index";
 import { PublicKey } from "@solana/web3.js";
+import { BN } from "bn.js";
 
 describe("SolanaDao", () => {
   test("calling getDao return a value", async () => {
@@ -11,7 +12,7 @@ describe("SolanaDao", () => {
         authority: new PublicKey("5We3g4zpinMcxSFrkvPWRzcSM5oRYxfWc9c8idefAQdi"),
         communityMint: new PublicKey("ukrn3bJz9dzSN2VpF76ytpNcugXHEy4VuRou7bpizgF"),
         councilMint: new PublicKey("cUAxX8fEXdisGSgRnfL4aH3WeomK5b277XqHHS4U9z7"),
-        minCommunityWeightToCreateGovernance: "1000000000000",
+        minCommunityTokensToCreateGovernance: "1000000000000",
         votingProposalCount: 0
     };
     expect(actualDao).toEqual(expectedDao);
