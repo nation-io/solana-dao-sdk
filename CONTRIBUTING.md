524930ca9d60ed0b8a0e9741e28860e7a8ace665
Ariel <arielsegura@users.noreply.github.com>
2022-11-04 16:56:04 +0100

    use localnet for integration tests (#12)

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -11,3 +11,9 @@ At least for now that the project is private and in its initial state we will fo
 [Work in Progress]
 
 We need to add the tools and frameworks we use or plan to add. ESLint, Prettier, Yarn, Lerna, Jest, Typescript and so on.
+
+## Integration Tests
+
+The integration tests run against a Solana validator that runs in a Docker container. The Docker image is maintained by @arielsegura and its entrypoint is `solana-test-validator`. We use a few options from this tool to copy the data we want from `mainnet-beta` to run a few assertions against real DAOs.
+
+To update the governance program on integration tests, please refer to this [question](https://solana.stackexchange.com/questions/3977/how-can-i-download-a-deployable-program-from-mainnet).
--- packages/solana-dao-sdk/src/index.test.ts
+++ packages/solana-dao-sdk/src/index.test.ts
@@ -1,10 +1,84 @@
 import { SolanaDao } from './index';
-import { PublicKey, Connection, clusterApiUrl } from '@solana/web3.js';
+import { PublicKey, Connection } from '@solana/web3.js';
 import { createWallet, randomId } from './test/utils';
+import { StartedTestContainer, GenericContainer } from 'testcontainers';
+import { governancePk } from './constants';
 
 describe('SolanaDao', () => {
+  let container: StartedTestContainer;
+  let connection: Connection;
+  const ukraineDAOPk = '5piGF94RbCqaogoFFWA9cYmt29qUpQejGCEjRKuwCz7d';
+  const ukraineDAOAuthorityPk = '5We3g4zpinMcxSFrkvPWRzcSM5oRYxfWc9c8idefAQdi';
+  const ukraineDAOCommunityMintPk =
+    'ukrn3bJz9dzSN2VpF76ytpNcugXHEy4VuRou7bpizgF';
+  const ukraineDAOCouncilMintPk = 'cUAxX8fEXdisGSgRnfL4aH3WeomK5b277XqHHS4U9z7';
+  const ukraineDAOTokenOwnerRecordOne =
+    '2kHNK3MvFotuUG78M61NHGy5aqAcsuNqtN9QtAE5ZiNu';
+  const ukraineDAOTokenOwnerRecordTwo =
+    'DwcS5tu22vFo7zk9YaX4eTfcrRN4kJLx4EGW7xJYWdTn';
+  const ukraineDAOTokenOwnerRecordThree =
+    'AkGpRaBxkezWJHrFjzZskHYTvUskWhVdvnDiLA2Kh1G5';
+  const ukraineDAOTokenOwnerRecordFour =
+    'AQ8QdVj4iZqPm25Cv81JQhG5J9Vco26vfRixncZZ5QTv';
+  const ukraineDAOTokenOwnerRecordFive =
+    '3MiaxuF6KFUuwKEUNid8trYowgTRfAR3aNBkrvbvUQwQ';
+  const ukraineDAOTokenOwnerRecordSix =
+    'FdcWVufA7qw3VfipQeBTMZTJqN8sC2fU3zCMB5mHYw8M';
+  const governanceProgramExecutableData =
+    'BZYjZ2ZbtAawP9WPt5yXszcoMWTvNFFX4AMbnDojtSGL';
+  jest.setTimeout(40000);
+  beforeAll(async () => {
+    container = await new GenericContainer(
+      'arisegura/solana-test-validator:v1.0.0',
+    )
+      .withCmd([
+        'solana-test-validator',
+        '--reset',
+        '--url',
+        'mainnet-beta',
+        '--clone',
+        ukraineDAOPk,
+        '--clone',
+        ukraineDAOAuthorityPk,
+        '--clone',
+        ukraineDAOCommunityMintPk,
+        '--clone',
+        ukraineDAOCouncilMintPk,
+        '--clone',
+        ukraineDAOTokenOwnerRecordOne,
+        '--clone',
+        ukraineDAOTokenOwnerRecordTwo,
+        '--clone',
+        ukraineDAOTokenOwnerRecordThree,
+        '--clone',
+        ukraineDAOTokenOwnerRecordFour,
+        '--clone',
+        ukraineDAOTokenOwnerRecordFive,
+        '--clone',
+        ukraineDAOTokenOwnerRecordSix,
+        '--clone',
+        governancePk.toBase58(),
+        '--clone',
+        governanceProgramExecutableData,
+      ])
+      .withExposedPorts(8899, 8900, 9900)
+      .withDefaultLogDriver()
+      .start();
+    const solanaTestValidatorRpc = `http://${container.getHost()}:${container.getMappedPort(
+      8899,
+    )}`;
+    const solanaTestValidatorWebSocket = `ws://${container.getHost()}:${container.getMappedPort(
+      8900,
+    )}`;
+
+    connection = new Connection(solanaTestValidatorRpc, {
+      commitment: 'confirmed',
+      wsEndpoint: solanaTestValidatorWebSocket,
+    });
+  });
+
   test('calling getDao return a value', async () => {
-    const client = new SolanaDao();
+    const client = new SolanaDao(connection);
     const actualDao = await client.getDao(
       new PublicKey('5piGF94RbCqaogoFFWA9cYmt29qUpQejGCEjRKuwCz7d'),
     );
@@ -22,43 +96,20 @@ describe('SolanaDao', () => {
     expect(actualDao).toEqual(expectedDao);
   });
 
-  test.skip('calling getMembers return values', async () => {
-    const client = new SolanaDao();
-    const actualMembers = await client.getMembers(
-      new PublicKey('5piGF94RbCqaogoFFWA9cYmt29qUpQejGCEjRKuwCz7d'),
-    );
-    expect(actualMembers).toContainEqual({
-      publicKey: new PublicKey('8nRQHj1RT7BQrhQ8jhRAoCUx8xrG4CxecNQRpyxvK7Zv'),
-    });
-    expect(actualMembers).toContainEqual({
-      publicKey: new PublicKey('LXppnYxEkEHaNsWCPkNwZzTXMQepLpi1iHytb3wZ5Qd'),
-    });
-    expect(actualMembers).toContainEqual({
-      publicKey: new PublicKey('5s9cM71STMqBYuk3oUztCSd2jjYh4G8RyxvFbdy3r4Qz'),
-    });
-    expect(actualMembers).toContainEqual({
-      publicKey: new PublicKey('AJh9U6Vc22BXXiaspkeaDJSd4KL1Lz9Qrmd59XTfLNwo'),
-    });
-    expect(actualMembers).toContainEqual({
-      publicKey: new PublicKey('EWcyEuktFMCUUzQ4vZPSjYtbBp3euwDJa8BN3MwZKVmY'),
-    });
-  });
-
   test('createDao creates a simpleDAO with default values', async () => {
-    const devnetConnection = new Connection(
-      clusterApiUrl('devnet'),
-      'confirmed',
-    );
-    const userWallet = await createWallet(devnetConnection);
-    const client = new SolanaDao(devnetConnection);
+    const userWallet = await createWallet(connection);
+    const client = new SolanaDao(connection);
     client.setWallet(userWallet);
 
     const daoName = `dao ${randomId()}`;
-
     const createdDao = await client.createDao(daoName);
-
     const retrievedDao = await client.getDao(createdDao.daoPk);
 
     expect(retrievedDao?.name).toBe(daoName);
+
+    const actualMembers = await client.getMembers(createdDao.daoPk);
+    expect(actualMembers).toContainEqual({
+      publicKey: userWallet.publicKey,
+    });
   });
 });
